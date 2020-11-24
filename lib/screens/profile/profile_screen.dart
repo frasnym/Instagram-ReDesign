25eb3792a582edf911e5ae81b2e2175528e653ae
frasnym <frastyawan.nym@gmail.com>
2020-11-24 10:53:50 +0800

    Profile Screen: Colapsed AppBar

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_redesign/constants.dart';
+import 'package:instagram_redesign/screens/profile/components/colapsed_appbar.dart';
 import 'package:instagram_redesign/widgets/bottom_nav_bar.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static const routeName = '/profile';
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen>
+    with SingleTickerProviderStateMixin {
+  ScrollController _scrollViewController;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollViewController = new ScrollController();
+    _tabController = new TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    _scrollViewController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +35,45 @@ class ProfileScreen extends StatelessWidget {
       appBar: buildAppBar(),
       body: Stack(
         children: [
-          // Bottom Navigation Bar
+          NestedScrollView(
+            controller: _scrollViewController,
+            headerSliverBuilder: (
+              BuildContext context,
+              bool innerBoxIsScrolled,
+            ) {
+              return <Widget>[
+                SliverAppBar(
+                  toolbarHeight: 120,
+                  // backgroundColor: kWhiteColor,
+                  title: ColapsedAppBar(),
+                  pinned: true, //<-- pinned to true
+                  floating: true, //<-- floating to true
+                  forceElevated:
+                      innerBoxIsScrolled, //<-- forceElevated to innerBoxIsScrolled
+                  bottom: new TabBar(
+                    tabs: <Tab>[
+                      new Tab(
+                        text: "STATISTICS",
+                        icon: new Icon(Icons.show_chart),
+                      ),
+                      new Tab(
+                        text: "HISTORY",
+                        icon: new Icon(Icons.history),
+                      ),
+                    ],
+                    controller: _tabController,
+                  ),
+                ),
+              ];
+            },
+            body: new TabBarView(
+              children: <Widget>[
+                Text('new StatisticsPage()'),
+                Text('new HistoryPage()'),
+              ],
+              controller: _tabController,
+            ),
+          ), // Bottom Navigation Bar
           BottomNavBar(pages: [false, false, false, false, true]),
         ],
       ),
